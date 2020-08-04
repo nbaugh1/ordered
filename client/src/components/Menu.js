ccc021c60a02d5ad590f6a72d6aa1da91e83d609
nbaugh1 <nbaugh1@gmail.com>
2020-08-04 16:40:42 -0400

    fetch menus from backend, load menus in menu component state, loading state

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table'
 
 export const Menu = () => {
-
+    const [menu, setMenu] = useState([])
+    const [menuLoading, setMenuLoading] = useState(true)
+    useEffect(() => {
+        window.fetch('/api/menus')
+            .then(response => response.json())
+            .then(json => setMenu(json))
+            .then(setMenuLoading(false))
+            .then(console.log(menu))
+            .catch(error => console.log(error))
+    }, [])
     return (
         <div>
-            <p>Menu</p>
-            <Table>
-
-            </Table>
+            {
+                menuLoading ?
+                    <div>
+                        <p>Menu</p>
+                        <Table>
+                            <h4>loading....</h4>
+                        </Table>
+                    </div>
+                    :
+                    <div>
+                        Menu Loaded!
+                        </div>
+            }
         </div>
+
     )
 }
