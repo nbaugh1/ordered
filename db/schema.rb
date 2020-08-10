0b2add47a7d2c450764708ffb8b5d945fd291e26
nbaugh1 <nbaugh1@gmail.com>
2020-08-10 14:08:13 -0400

    drop and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_195720) do
+ActiveRecord::Schema.define(version: 2020_08_10_180455) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,6 +65,16 @@ ActiveRecord::Schema.define(version: 2020_08_05_195720) do
 
   create_table "orders", force: :cascade do |t|
     t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
