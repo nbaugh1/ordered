39c2d88d96e07df5bef78870246274cbbd89bb5a
nbaugh1 <nbaugh1@gmail.com>
2020-08-10 14:07:25 -0400

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: /@/
 end
