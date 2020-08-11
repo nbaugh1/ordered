2d7e0829927e316c11633dd298177e9bd43d30bc
nbaugh1 <nbaugh1@gmail.com>
2020-08-11 12:20:16 -0400

    add password confirmation back to to permitted params for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApiController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
