e7fa2595d67fcdf920320c063662e2690d7de965
nbaugh1 <nbaugh1@gmail.com>
2020-08-14 16:19:09 -0400

    add accoridon to menus page

--- client/src/components/MenuType.js
+++ client/src/components/MenuType.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 const MenuType = ({ menu }) => {
     return (
         <div>
             <h1>{menu.name}</h1>
             <h3>Items:</h3>
-            {menu.items.map((item) =>
-                <p>{item.name} : {item.description} - ${item.price}</p>
-            )}
+            <Accordion defaultActiveKey='0'>
+                {menu.items.map((item) =>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey={item.id}>
+                            {item.name}                            ${item.price}
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey={item.id}>
+                            <Card.Body>{item.description}</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                )}
+            </Accordion>
+
         </div>
+
+
     )
 }
 
 export default MenuType
+
